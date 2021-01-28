8d1407421f30b636329fa01a285e6818cbf89365
Valerie <valerie_hotmail@hotmail.com>
2021-01-28 19:29:19 +0100

    First commit

---
+++ src/com/utils/ElGamal.java
@@ -0,0 +1,13 @@
+/*
+ * ElGamal.java
+ * ElGamal
+ *
+ * Created by Valerie Marissens Cueva on 28/1/2021.
+ * Copyright (c) 2021.
+ *
+ */
+
+package com.utils;
+
+public class ElGamal {
+}
---
+++ src/com/utils/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Main.java
+ * ElGamal
+ *
+ * Created by Valerie Marissens Cueva on 28/1/2021.
+ * Copyright (c) 2021.
+ *
+ */
+
+package com.utils;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
