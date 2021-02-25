49fe913626a5379680607df0be803bb737e45601
Lothary <nicolas54250@hotmail.fr>
2021-02-25 17:35:01 +0100

    ElGamal fonctionnel     - keyGen()     - Encrypt()     - Decrypt()

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@
  *
  */
 
+import elgamal.ElGamal;
 import elgamal.Euclide;
 import elgamal.ModularExponentiation;
 import elgamal.exceptions.EuclideException;
@@ -14,11 +15,12 @@ import elgamal.exceptions.EuclideException;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchProviderException, NoSuchAlgorithmException, EuclideException {
         String hexa = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF";
         BigInteger p = new BigInteger(hexa, 16);
         BigInteger g = BigInteger.valueOf(2);
@@ -29,7 +31,7 @@ public class Main {
         BigInteger b = BigInteger.valueOf(5);
         BigInteger c = BigInteger.valueOf(3);
 
-        System.out.println(modularExponentiation.expMod(c,b,3));
+        //System.out.println(modularExponentiation.expMod(c,b,3));
 
         //try {
             //euclide.testTenThousandTimes(p);
@@ -37,5 +39,33 @@ public class Main {
         //} catch (NoSuchProviderException | NoSuchAlgorithmException | EuclideException e) {
         //    e.printStackTrace();
         //}
+
+        ElGamal elGamal = new ElGamal();
+
+
+
+        BigInteger[] keys = elGamal.KeyGen(p,g);
+        BigInteger bobPrivateKey = keys[0];
+        BigInteger bobPublicKey = keys[1];
+
+        //System.out.println("La clé privée de Bob est : " + bobPrivateKey);
+        //System.out.println("La clé publique de Bob est : " + bobPublicKey + "\n");
+
+        BigInteger message = BigInteger.valueOf(123456789);
+        System.out.println("Le message est : " + message.intValue());
+
+        BigInteger[] encrypt = elGamal.Encrypt(p,g, bobPublicKey, message);
+        BigInteger messageChiffreC = encrypt[0];
+        BigInteger messageChiffreB = encrypt[1];
+
+        System.out.println("Le message chiffré est : \n" );
+        System.out.println("C  : " + messageChiffreC.intValue());
+        System.out.println("B  : " + messageChiffreB.intValue());
+
+        message = elGamal.Decrypt(messageChiffreC,messageChiffreB, bobPrivateKey.intValue(), p);
+        System.out.println("Le message déchiffré est : " + message.intValue());
+
+
+
     }
 }
--- src/elgamal/ElGamal.java
+++ src/elgamal/ElGamal.java
@@ -9,5 +9,65 @@
 
 package elgamal;
 
+import elgamal.exceptions.EuclideException;
+
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
 public class ElGamal {
+
+    private ModularExponentiation modularExponentiation;
+    private Euclide euclide;
+
+    public ElGamal(){
+        modularExponentiation = new ModularExponentiation();
+        euclide = new Euclide();
+    }
+
+    public BigInteger[] KeyGen(BigInteger p, BigInteger g) throws NoSuchProviderException, NoSuchAlgorithmException {
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        int x = Math.abs(random.nextInt());
+        // int x = Math.abs(random.nextInt((p.intValue() - 2)) - 2 + 1) + 2;
+
+        BigInteger privateKey = BigInteger.valueOf(x);
+        BigInteger publicKey = modularExponentiation.expMod(p,g,x);
+
+
+        BigInteger[] results = new BigInteger[2];
+        results[0] = privateKey;
+        results[1] = publicKey;
+
+        return results;
+    }
+
+    public BigInteger[] Encrypt(BigInteger p , BigInteger g, BigInteger publicKey, BigInteger m) throws NoSuchProviderException, NoSuchAlgorithmException {
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        //int r = Math.abs(random.nextInt((p.intValue() - 2)) - 2 + 1) + 2;
+        int r = Math.abs(random.nextInt());
+        BigInteger y = modularExponentiation.expMod(p, publicKey, r);
+
+
+        BigInteger C = (m.multiply(y)).mod(p);
+        BigInteger B = modularExponentiation.expMod(p,g, r);
+
+        BigInteger[] results = new BigInteger[2];
+        results[0] = C;
+        results[1] = B;
+
+        return results;
+    }
+
+    public BigInteger Decrypt(BigInteger C, BigInteger B, int secretKey, BigInteger p) throws EuclideException {
+
+        BigInteger D = modularExponentiation.expMod(p,B,secretKey);
+        BigInteger m = (C.multiply(euclide.euclide(D,p)[0])).mod(p);
+
+        return m;
+    }
+
+
 }
